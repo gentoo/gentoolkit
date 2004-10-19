378c18098bf33a61a4979fc1f04dd7cecf1972cd
karltk <karltk@gentoo.org>
2004-10-19 23:50:20 +0000

    Added case for sudo
    svn path=/; revision=163

--- trunk/src/equery/tests/test-check.sh
+++ trunk/src/equery/tests/test-check.sh
@@ -20,8 +20,20 @@ test_check() {
 	assert_ge ${FUNCNAME} ${x} 1
 }
 
+test_check_permissions() {
+	equery check sudo > ${tmpfile}
+
+	x=$(grep "app-admin/sudo" ${tmpfile} | wc -l)
+	
+	assert_ge ${FUNCNAME} ${x} 1
+	
+	x=$(egrep "[0-9]+ out of [0-9]+" ${tmpfile} | wc -l)
+	assert_ge ${FUNCNAME} ${x} 1
+}
+
 # Run tests
 
 test_check
+test_check_permissions
 
 rm -f ${tmpfile}
