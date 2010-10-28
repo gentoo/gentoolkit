7736c7d22e9c5692fef8b73a3a27bf9c6c101918
scarabeus <scarabeus@gentoo.org>
2010-10-28 21:18:06 +0000

    Initial commit to add equery keywords support for executing eshowkw.
    svn path=/trunk/gentoolkit/; revision=837

---
+++ pym/gentoolkit/equery/keywords.py
@@ -0,0 +1,7 @@
+# Copyright 2010 Gentoo Foundation
+# Distributed under the terms of the GNU General Public License v2
+
+import sys
+from ...gentoolkit.eshowkw import main as emain
+
+emain(sys.argv)
