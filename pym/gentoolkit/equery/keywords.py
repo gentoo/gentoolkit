a09461df542c3a56f9d2e6a8d15fbf1e4f8f4509
scarabeus <scarabeus@gentoo.org>
2010-10-28 21:31:47 +0000

    Fix import path again. Process only proper passed args in required function.
    svn path=/trunk/gentoolkit/; revision=838

--- pym/gentoolkit/equery/keywords.py
+++ pym/gentoolkit/equery/keywords.py
@@ -2,6 +2,8 @@
 # Distributed under the terms of the GNU General Public License v2
 
 import sys
-from ...gentoolkit.eshowkw import main as emain
+from gentoolkit.eshowkw import main as emain
 
+# we have equery as first argument instead of the scriptname
+# so we will just ommit it
 emain(sys.argv)
