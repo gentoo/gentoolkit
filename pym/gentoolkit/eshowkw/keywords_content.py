939911f82591418f0517cd7e30385e5e1b0c9eac
scarabeus <scarabeus@gentoo.org>
2010-11-02 14:07:08 +0000

    Revert altering keywords_content. This change was not ready yet.
    svn path=/trunk/gentoolkit/; revision=844

--- pym/gentoolkit/eshowkw/keywords_content.py
+++ pym/gentoolkit/eshowkw/keywords_content.py
@@ -155,11 +155,7 @@ class keywords_content:
 	def __checkExist(self, pdb, package):
 		"""Check if specified package even exists."""
 		try:
-			cp = port.dep_expand(package, mydb=pdb, settings=pdb.settings).cp
-			matches = []
-			for repo in pdb.porttrees:
-				for cpv in pdb.cp_list(mycp=cp,mytree=[repo]):
-					matches.append((cpv, repo))
+			matches = pdb.xmatch('match-all', package)
 		except port.exception.AmbiguousPackageName as Arg:
 			msg_err = 'Ambiguous package name "%s".\n' % package
 			found = 'Possibilities: %s' % Arg
