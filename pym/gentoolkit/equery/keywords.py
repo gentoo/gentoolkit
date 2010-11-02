6ed89d98b367561dc0d56b916cb459b57607c22f
scarabeus <scarabeus@gentoo.org>
2010-11-02 14:14:31 +0000

    Fix typos in previous commit.
    svn path=/trunk/gentoolkit/; revision=845

--- pym/gentoolkit/equery/keywords.py
+++ pym/gentoolkit/equery/keywords.py
@@ -2,8 +2,7 @@
 # Copyright 2010 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
 
-import sys
 from gentoolkit.eshowkw import main as emain
 
 def main(input_args):
-	args.extend(input_args)
+	emain(input_args)
