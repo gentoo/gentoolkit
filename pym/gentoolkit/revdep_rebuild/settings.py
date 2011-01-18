74b8c16fa6f8ccb173b480093317c9365e66250a
Slawek <lis.slawek@gmail.com>
2011-01-18 07:18:42 +0100

    Some fixes after file split. Removed unwanted files. Moved settings into module

---
+++ pym/gentoolkit/revdep_rebuild/settings.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+
+import os
+import portage
+
+SETTINGS = {
+        'DEFAULT_LD_FILE': os.path.join(portage.root, 'etc/ld.so.conf'),
+        'DEFAULT_ENV_FILE': os.path.join(portage.root, 'etc/profile.env'),
+        'REVDEP_CONFDIR': os.path.join(portage.root, 'etc/revdep-rebuild/'),
+        'PKG_DIR': os.path.join(portage.root, 'var/db/pkg/'),
+        'DEFAULT_TMP_DIR': '/tmp/revdep-rebuild', #cache default location
+
+
+        'USE_TMP_FILES': True, #if program should use temporary files from previous run
+        'CMD_MAX_ARGS': 1000, # number of maximum allowed files to be parsed at once
+
+        'PRETEND': False,     #pretend only
+        'EXACT': False,      #exact package version
+        'USE_TMP_FILES': True, #if program should use temporary files from previous run
+
+        'IS_DEV': True,       #True for dev. version, False for stable
+                #used when IS_DEV is True, False forces to call emerge with --pretend
+                # can be set True from the cli with the --no-pretend option
+        'NO_PRETEND': False,
+        'VERBOSITY': 1,
+    }
