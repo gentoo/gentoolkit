56cb3e4969a08b3f0d30ea0e1992ae4dd9b42bd3
Tomas Chvatal <scarabeus@gentoo.org>
2011-01-31 00:38:03 +0100

    Align the suffix for revision correctly.

--- pym/gentoolkit/eshowkw/keywords_content.py
+++ pym/gentoolkit/eshowkw/keywords_content.py
@@ -113,9 +113,11 @@ class keywords_content:
 			"""Prefix and suffix version with string based on whether version is installed or masked and its revision."""
 			mask = self.__getMaskStatus(cpv)
 			install = self.__getInstallStatus(cpv)
-			suffixlen = revlength - len(self.__getRevision(cpv))
+			# calculate suffix length
+			currevlen = len(self.__getRevision(cpv))
+			suffixlen = revlength - currevlen
 			# +1 required for the dash in revision
-			if suffixlen != 0:
+			if suffixlen != 0 and currevlen == 0:
 				suffixlen = suffixlen + 1
 			suffix = ''
 			for x in range(suffixlen):
