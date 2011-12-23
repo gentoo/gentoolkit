a6b7a7adced1a55327cd947c424987899e6f48ae
Brian Dolbec <dolsen@gentoo.org>
2011-12-22 22:24:53 -0800

    Fix equery's term_width too.

--- pym/gentoolkit/equery/__init__.py
+++ pym/gentoolkit/equery/__init__.py
@@ -207,7 +207,7 @@ def initialize_configuration():
 
 	# Get terminal size
 	term_width = pp.output.get_term_size()[1]
-	if term_width == -1:
+	if term_width < 1:
 		# get_term_size() failed. Set a sane default width:
 		term_width = 80
 
