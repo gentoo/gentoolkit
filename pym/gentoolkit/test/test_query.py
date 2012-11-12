096b295fe7bff8a0e83047cde0b5ce350d84d50f
W. Trevor King <wking@tremily.us>
2012-11-11 22:06:49 -0500

    test_query: fix test_main and mark so nosetests will ignore it

--- pym/gentoolkit/test/test_query.py
+++ pym/gentoolkit/test/test_query.py
@@ -1,10 +1,6 @@
 import unittest
 import warnings
 from tempfile import NamedTemporaryFile, mktemp
-try:
-	from test import test_support
-except ImportError:
-	from test import support as test_support
 
 from gentoolkit import query
 from gentoolkit import errors
@@ -102,7 +98,9 @@ class TestQuery(unittest.TestCase):
 
 
 def test_main():
-	test_support.run_unittest(TestGentoolkitHelpers2)
+	suite = unittest.TestLoader().loadTestsFromTestCase(TestQuery)
+	unittest.TextTestRunner(verbosity=2).run(suite)
+test_main.__test__ = False
 
 
 if __name__ == '__main__':
