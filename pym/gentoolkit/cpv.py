e8db7d799af467c3eff7ec7beb5eee3e3b1da538
W. Trevor King <wking@tremily.us>
2012-11-11 21:51:58 -0500

    cpv: remove shebang from module without __name__ == '__main__' block

--- pym/gentoolkit/cpv.py
+++ pym/gentoolkit/cpv.py
@@ -1,5 +1,3 @@
-#!/usr/bin/python
-#
 # Copyright(c) 2005 Jason Stubbs <jstubbs@gentoo.org>                         
 # Copyright(c) 2005-2006 Brian Harring <ferringb@gmail.com>
 # Copyright(c) 2009-2010 Gentoo Foundation
