840bf7e8df3c86e161f7855a37b008b5cf16f2d8
Mike Frysinger <vapier@gentoo.org>
2014-01-19 23:38:10 -0500

    ekeyword: rewrite in python
    A clean rewrite from scratch with unittests all that goodness.
    
    URL: https://bugs.gentoo.org/267565

---
+++ src/ekeyword/ekeyword.py
@@ -0,0 +1,433 @@
+#!/usr/bin/python
+# Copyright 2014 Gentoo Foundation
+# Distributed under the terms of the GNU General Public License v2
+# Written by Mike Frysinger <vapier@gentoo.org>
+
+"""Manage KEYWORDS in ebuilds easily.
+
+This tool provides a simple way to add or update KEYWORDS in a set of ebuilds.
+Each command-line argument is processed in order, so that keywords are added to
+the current list as they appear, and ebuilds are processed as they appear.
+
+Instead of specifying a specific arch, it's possible to use the word "all".
+This causes the change to apply to all keywords presently specified in the
+ebuild.
+
+The ^ leader instructs ekeyword to remove the specified arch.
+
+Examples:
+
+  # Mark all existing arches in the ebuild as stable.
+  $ ekeyword all foo-1.ebuild
+
+  # Mark arm as stable and x86 as unstable.
+  $ ekeyword arm ~x86 foo-1.ebuild
+
+  # Mark hppa as unsupported (explicitly adds -hppa).
+  $ ekeyword -hppa foo-1.ebuild
+
+  # Delete alpha keywords from all ebuilds.
+  $ ekeyword ^alpha *.ebuild
+
+  # Mark sparc as stable for foo-1 and m68k as unstable for foo-2.
+  $ ekeyword sparc foo-1.ebuild ~m68k foo-2.ebuild
+
+  # Mark s390 as the same state as amd64.
+  $ ekeyword s390=amd64 foo-1.ebuild
+"""
+
+from __future__ import print_function
+
+import argparse
+import collections
+import difflib
+import os
+import re
+import sys
+
+import portage
+from portage.output import colorize, nocolor
+
+
+VERSION = '1.0 awesome'
+
+Op = collections.namedtuple('Op', ('op', 'arch', 'ref_arch'))
+
+
+def keyword_to_arch(keyword):
+	"""Given a keyword, strip it down to its arch value
+
+	When an ARCH shows up in KEYWORDS, it may have prefixes like ~ or -.
+	Strip all that cruft off to get back to the ARCH.
+	"""
+	return keyword.lstrip('-~')
+
+
+def sort_keywords(arches):
+	"""Sort |arches| list in the order developers expect"""
+	keywords = []
+
+	# Globs always come first.
+	for g in ('-*', '*', '~*'):
+		if g in arches:
+			arches.remove(g)
+			keywords.append(g)
+
+	def arch_cmp(a1, a2):
+		# Sort independent of leading marker (~ or -).
+		a1 = keyword_to_arch(a1)
+		a2 = keyword_to_arch(a2)
+
+		# If a keyword has a "-" in it, then it always comes after ones
+		# that do not.  We want things like alpha/mips/sparc showing up
+		# before amd64-fbsd and amd64-linux.
+		if '-' in a1 and not '-' in a2:
+			return 1
+		elif '-' not in a1 and '-' in a2:
+			return -1
+		else:
+			return cmp(a1, a2)
+
+	keywords += sorted(arches, cmp=arch_cmp)
+
+	return keywords
+
+
+def diff_keywords(old_keywords, new_keywords, format='color-inline'):
+	"""Show pretty diff between list of keywords"""
+	def show_diff(s):
+		output = ''
+
+		for tag, i0, i1, j0, j1 in s.get_opcodes():
+
+			if tag == 'equal':
+				output += s.a[i0:i1]
+
+			if tag in ('delete', 'replace'):
+				o = s.a[i0:i1]
+				if format == 'color-inline':
+					o = colorize('bg_darkred', o)
+				else:
+					o = '-{%s}' % o
+				output += o
+
+			if tag in ('insert', 'replace'):
+				o = s.b[j0:j1]
+				if format == 'color-inline':
+					o = colorize('bg_darkgreen', o)
+				else:
+					o = '+{%s}' % o
+				output += o
+
+		return output
+
+	sold = ' '.join(old_keywords)
+	snew = ' '.join(new_keywords)
+	s = difflib.SequenceMatcher(str.isspace, sold, snew, autojunk=False)
+	return show_diff(s)
+
+
+def process_keywords(keywords, ops, arch_status=None):
+	"""Process |ops| for |keywords|"""
+	new_keywords = set(keywords).copy()
+
+	# Process each op one at a time.
+	for op, oarch, refarch in ops:
+		# Figure out which keywords we need to modify.
+		if oarch == 'all':
+			if not arch_status:
+				raise ValueError('unable to process "all" w/out profiles.desc')
+			old_arches = set([keyword_to_arch(a) for a in new_keywords])
+			if op is None:
+				# Process just stable keywords.
+				arches = [k for k, v in arch_status.items()
+				          if v == 'stable' and k in old_arches]
+			else:
+				# Process all possible keywords.  We use the arch_status as a
+				# master list.  If it lacks some keywords, then we might miss
+				# somethings here, but not much we can do.
+				arches = set(arch_status.keys()) | old_arches
+		else:
+			arches = (oarch,)
+
+		if refarch:
+			# Figure out the state for this arch based on the reference arch.
+			# TODO: Add support for "all" keywords.
+			# XXX: Should this ignore the '-' state ?  Does it make sense to
+			#      sync e.g. "s390" to "-ppc" ?
+			refkeyword = [x for x in new_keywords if refarch == keyword_to_arch(x)]
+			if not refkeyword:
+				op = '^'
+			elif refkeyword[0].startswith('~'):
+				op = '~'
+			elif refkeyword[0].startswith('-'):
+				op = '-'
+			else:
+				op = None
+
+		# Finally do the actual update of the keywords list.
+		for arch in arches:
+			new_keywords -= set(['%s%s' % (x, arch) for x in ('', '~', '-')])
+
+			if op is None:
+				new_keywords.add(arch)
+			elif op in ('~', '-'):
+				new_keywords.add('%s%s' % (op, arch))
+			elif op == '^':
+				# Already deleted.  Whee.
+				pass
+			else:
+				raise ValueError('unknown operation %s' % op)
+
+	return new_keywords
+
+
+def process_content(ebuild, data, ops, arch_status=None, verbose=False,
+                    quiet=False, format='color-inline'):
+	"""Process |ops| for |data|"""
+	# Set up the user display style based on verbose/quiet settings.
+	if verbose:
+		disp_name = ebuild
+		def logit(msg):
+			print('%s: %s' % (disp_name, msg))
+	elif quiet:
+		def logit(msg):
+			pass
+	else:
+		# Chop the full path and the .ebuild suffix.
+		disp_name = os.path.basename(ebuild)[:-7]
+		def logit(msg):
+			print('%s: %s' % (disp_name, msg))
+
+	# Match any KEYWORDS= entry that isn't commented out.
+	keywords_re = re.compile(r'^([^#]*\bKEYWORDS=)([\'"])(.*)(\2)(.*)')
+	updated = False
+	content = []
+
+	# Walk each line of the ebuild looking for KEYWORDS to process.
+	for line in data:
+		m = keywords_re.match(line)
+		if not m:
+			content.append(line)
+			continue
+
+		# Ok, we've got it, now let's process things.
+		old_keywords = set(m.group(3).split())
+		new_keywords = process_keywords(
+			old_keywords, ops, arch_status=arch_status)
+
+		# Finally let's present the results to the user.
+		if new_keywords != old_keywords:
+			# Only do the diff work if something actually changed.
+			updated = True
+			old_keywords = sort_keywords(old_keywords)
+			new_keywords = sort_keywords(new_keywords)
+			line = '%s"%s"%s\n' % (m.group(1), ' '.join(new_keywords),
+			                       m.group(5))
+			if format in ('color-inline', 'inline'):
+				logit(diff_keywords(old_keywords, new_keywords, format=format))
+			else:
+				if format == 'long-multi':
+					logit(' '.join(['%*s' % (len(keyword_to_arch(x)) + 1, x)
+					                for x in old_keywords]))
+					logit(' '.join(['%*s' % (len(keyword_to_arch(x)) + 1, x)
+					                for x in new_keywords]))
+				else:
+					deleted_keywords = [x for x in old_keywords
+					                    if x not in new_keywords]
+					logit('--- %s' % ' '.join(deleted_keywords))
+					added_keywords = [x for x in new_keywords
+					                  if x not in old_keywords]
+					logit('+++ %s' % ' '.join(added_keywords))
+
+		content.append(line)
+
+	if not updated:
+		logit('no updates')
+
+	return updated, content
+
+
+def process_ebuild(ebuild, ops, arch_status=None, verbose=False, quiet=False,
+                   dry_run=False, format='color-inline'):
+	"""Process |ops| for |ebuild|"""
+	with open(ebuild, 'rb') as f:
+		updated, content = process_content(
+			ebuild, f, ops, arch_status=arch_status,
+			verbose=verbose, quiet=quiet, format=format)
+		if updated and not dry_run:
+			with open(ebuild, 'wb') as f:
+				f.writelines(content)
+
+
+def load_profile_data(portdir=None, repo='gentoo'):
+	"""Load the list of known arches from the tree"""
+	if portdir is None:
+		portdir = portage.db['/']['vartree'].settings.repositories[repo].location
+
+	arch_status = {}
+
+	try:
+		arch_list = os.path.join(portdir, 'profiles', 'arch.list')
+		with open(arch_list) as f:
+			for line in f:
+				line = line.split('#', 1)[0].strip()
+				if line:
+					arch_status[line] = None
+	except IOError:
+		pass
+
+	try:
+		profile_status = {
+			'stable': 0,
+			'dev': 1,
+			'exp': 2,
+			None: 3,
+		}
+		profiles_list = os.path.join(portdir, 'profiles', 'profiles.desc')
+		with open(profiles_list) as f:
+			for line in f:
+				line = line.split('#', 1)[0].split()
+				if line:
+					arch, profile, status = line
+					arch_status.setdefault(arch, status)
+					curr_status = profile_status[arch_status[arch]]
+					new_status = profile_status[status]
+					if new_status < curr_status:
+						arch_status[arch] = status
+	except IOError:
+		pass
+
+	if arch_status:
+		arch_status['all'] = None
+	else:
+		print('warning: could not read profile files: %s' % arch_list, file=sys.stderr)
+		print('warning: will not be able to verify args are correct', file=sys.stderr)
+
+	return arch_status
+
+
+def arg_to_op(arg):
+	"""Convert a command line |arg| to an Op"""
+	arch_prefixes = ('-', '~', '^')
+
+	op = None
+	arch = arg
+	refarch = None
+
+	if arg and arg[0] in arch_prefixes:
+		op, arch = arg[0], arg[1:]
+
+	if '=' in arch:
+		if not op is None:
+			raise ValueError('Cannot use an op and a refarch')
+		arch, refarch = arch.split('=', 1)
+
+	return Op(op, arch, refarch)
+
+
+def args_to_work(args, arch_status=None, repo='gentoo'):
+	"""Process |args| into a list of work itmes (ebuild/arches to update)"""
+	work = []
+	todo_arches = []
+	last_todo_arches = None
+
+	for arg in args:
+		if arg.endswith('.ebuild'):
+			if not todo_arches:
+				todo_arches = last_todo_arches
+			if not todo_arches:
+				raise ValueError('missing arches to process for %s' % arg)
+			work.append([arg, todo_arches])
+			last_todo_arches = todo_arches
+			todo_arches = []
+		else:
+			op = arg_to_op(arg)
+			if not arch_status or op.arch in arch_status:
+				todo_arches.append(op)
+			else:
+				raise ValueError('unknown arch/argument: %s' % arg)
+
+	if todo_arches:
+		raise ValueError('missing ebuilds to process!')
+
+	return work
+
+
+def get_parser():
+	"""Return an argument parser for ekeyword"""
+	parser = argparse.ArgumentParser(
+		description=__doc__,
+		formatter_class=argparse.RawDescriptionHelpFormatter)
+	parser.add_argument('-n', '--dry-run', default=False, action='store_true',
+		help='Show what would be changed, but do not commit')
+	parser.add_argument('-v', '--verbose', default=False, action='store_true',
+		help='Be verbose while processing things')
+	parser.add_argument('-q', '--quiet', default=False, action='store_true',
+		help='Be quiet while processing things (only show errors)')
+	parser.add_argument('--format', default='auto',
+		choices=('auto', 'color-inline', 'inline', 'short-multi', 'long-multi'),
+		help='Selet output format for showing differences')
+	parser.add_argument('-V', '--version', default=False, action='store_true',
+		help='Show version information')
+	return parser
+
+
+def main(argv):
+	if argv is None:
+		argv = sys.argv[1:]
+
+	# Extract the args ourselves.  This is to allow things like -hppa
+	# without tripping over the -h/--help flags.  We can't use the
+	# parse_known_args function either.
+	# This sucks and really wish we didn't need to do this ...
+	parse_args = []
+	work_args = []
+	while argv:
+		arg = argv.pop(0)
+		if arg.startswith('--'):
+			if arg == '--':
+				work_args += argv
+				break
+			else:
+				parse_args.append(arg)
+			# Handle flags that take arguments.
+			if arg in ('--format',):
+				if argv:
+					parse_args.append(argv.pop(0))
+		elif arg[0] == '-' and len(arg) == 2:
+			parse_args.append(arg)
+		else:
+			work_args.append(arg)
+
+	parser = get_parser()
+	opts = parser.parse_args(parse_args)
+	if opts.version:
+		print('version: %s' % VERSION)
+		return os.EX_OK
+	if not work_args:
+		parser.error('need arches/ebuilds to process')
+
+	if opts.format == 'auto':
+		if not portage.db['/']['vartree'].settings.get('NOCOLOR', 'false').lower() in ('no', 'false'):
+			nocolor()
+			opts.format = 'short'
+		else:
+			opts.format = 'color-inline'
+
+	arch_status = load_profile_data()
+	try:
+		work = args_to_work(work_args, arch_status=arch_status)
+	except ValueError as e:
+		parser.error(e)
+
+	for ebuild, ops in work:
+		process_ebuild(ebuild, ops, arch_status=arch_status,
+		               verbose=opts.verbose, quiet=opts.quiet,
+		               dry_run=opts.dry_run, format=opts.format)
+
+	return os.EX_OK
+
+
+if __name__ == '__main__':
+	sys.exit(main(sys.argv[1:]))
---
+++ src/ekeyword/ekeyword_unittest.py
@@ -0,0 +1,368 @@
+#!/usr/bin/python
+# Copyright 2014 Gentoo Foundation
+# Distributed under the terms of the GNU General Public License v2
+# Written by Mike Frysinger <vapier@gentoo.org>
+
+"""Unittests for ekeyword"""
+
+import os
+import tempfile
+import unittest
+
+import ekeyword
+
+
+TESTDIR = os.path.join(os.path.dirname(__file__), 'tests')
+
+
+class TestSortKeywords(unittest.TestCase):
+	"""Tests for sort_keywords"""
+
+	def _test(self, input_data, exp_data):
+		output_data = ekeyword.sort_keywords(input_data.split())
+		self.assertEqual(exp_data.split(), output_data)
+
+	def testNull(self):
+		self._test('', '')
+		self._test('   		 ', '')
+
+	def testGlob(self):
+		self._test('* arm', '* arm')
+		self._test('arm -* x86', '-* arm x86')
+		self._test('hppa ~* amd64', '~* amd64 hppa')
+
+	def testNonLinux(self):
+		self._test('arm-linux alpha amd64-fbsd hppa',
+		           'alpha hppa amd64-fbsd arm-linux')
+
+	def testPrefixes(self):
+		self._test('-hppa arm ~alpha -* ~arm-linux',
+		           '-* ~alpha arm -hppa ~arm-linux')
+
+
+class TestDiffKeywords(unittest.TestCase):
+	"""Tests for diff_keywords"""
+
+	def testEmpty(self):
+		"""Test when there is no content to diff"""
+		ret = ekeyword.diff_keywords([], [])
+		self.assertEqual(ret, '')
+
+	def testSame(self):
+		"""Test when there is no difference"""
+		ret = ekeyword.diff_keywords(['a b c'], ['a b c'])
+		self.assertEqual(ret, 'a b c')
+
+	def testInsert(self):
+		"""Test when content is simply added"""
+		ret = ekeyword.diff_keywords(['a'], ['~a'])
+		self.assertNotEqual(ret, '')
+
+	def testDelete(self):
+		"""Test when content is simply deleted"""
+		ret = ekeyword.diff_keywords(['~a'], ['a'])
+		self.assertNotEqual(ret, '')
+
+	def testReplace(self):
+		"""Test when some content replaces another"""
+		ret = ekeyword.diff_keywords(['~a'], ['-a'])
+		self.assertNotEqual(ret, '')
+
+	def _testSmokeFormat(self, format):
+		return ekeyword.diff_keywords(
+			['~a', 'b', '-abcde'],
+			['a', '-b', '-abxde'], format=format)
+
+	def testSmokeFormatColor(self):
+		"""Run a full smoke test for color-inline format"""
+		ret = self._testSmokeFormat('color-inline')
+		self.assertNotEqual(ret, '')
+
+	def testSmokeFormatNoColor(self):
+		"""Run a full smoke test for non-color-inline format"""
+		self._testSmokeFormat('nocolor')
+
+
+class TestProcessKeywords(unittest.TestCase):
+	"""Tests for process_keywords"""
+
+	def _test(self, keywords, ops, exp, arch_status=None):
+		# This func doesn't return sorted results (which is fine),
+		# so do so ourselves to get stable tests.
+		ret = ekeyword.process_keywords(
+			keywords.split(), ops, arch_status=arch_status)
+		self.assertEqual(sorted(ret), sorted(exp.split()))
+
+	def testAdd(self):
+		ops = (
+			ekeyword.Op(None, 'arm', None),
+			ekeyword.Op('~', 's390', None),
+			ekeyword.Op('-', 'sh', None),
+		)
+		self._test('moo', ops, 'arm ~s390 -sh moo')
+
+	def testModify(self):
+		ops = (
+			ekeyword.Op(None, 'arm', None),
+			ekeyword.Op('~', 's390', None),
+			ekeyword.Op('-', 'sh', None),
+		)
+		self._test('~arm s390 ~sh moo', ops, 'arm ~s390 -sh moo')
+
+	def testDelete(self):
+		ops = (
+			ekeyword.Op('^', 'arm', None),
+			ekeyword.Op('^', 's390', None),
+			ekeyword.Op('^', 'x86', None),
+		)
+		self._test('arm -s390 ~x86 bar', ops, 'bar')
+
+	def testSync(self):
+		ops = (
+			ekeyword.Op('=', 'arm64', 'arm'),
+			ekeyword.Op('=', 'ppc64', 'ppc'),
+			ekeyword.Op('=', 'amd64', 'x86'),
+			ekeyword.Op('=', 'm68k', 'mips'),
+			ekeyword.Op('=', 'ia64', 'alpha'),
+			ekeyword.Op('=', 'sh', 'sparc'),
+			ekeyword.Op('=', 's390', 's390x'),
+			ekeyword.Op('=', 'boo', 'moo'),
+		)
+		self._test(
+			'arm64 arm '
+			'~ppc64 ~ppc '
+			'~amd64 x86 '
+			'm68k ~mips '
+			'-ia64 alpha '
+			'sh -sparc '
+			's390 '
+			'moo ',
+			ops,
+			'arm64 arm ~ppc64 ~ppc amd64 x86 ~m68k ~mips ia64 alpha '
+			'-sh -sparc boo moo')
+
+	def testAllNoStatus(self):
+		ops = (
+			ekeyword.Op(None, 'all', None),
+		)
+		self.assertRaises(ValueError, self._test, '', ops, '')
+
+	def testAllStable(self):
+		ops = (
+			ekeyword.Op(None, 'all', None),
+		)
+		arch_status = {
+			'alpha': None,
+			'arm': 'stable',
+			'arm64': 'exp',
+			'm68k': 'dev',
+		}
+		self._test('* ~alpha ~arm ~arm64 ~m68k ~mips ~arm-linux', ops,
+		           '* ~alpha arm ~arm64 ~m68k ~mips ~arm-linux', arch_status)
+
+	def testAllUnstable(self):
+		ops = (
+			ekeyword.Op('~', 'all', None),
+		)
+		arch_status = {
+			'alpha': None,
+			'arm': 'stable',
+			'arm64': 'exp',
+			'm68k': 'dev',
+		}
+		self._test('alpha arm arm64 m68k mips arm-linux', ops,
+		           '~alpha ~arm ~arm64 ~m68k ~mips ~arm-linux', arch_status)
+
+	def testAllMultiUnstableStable(self):
+		ops = (
+			ekeyword.Op('~', 'all', None),
+			ekeyword.Op(None, 'all', None),
+		)
+		arch_status = {
+			'alpha': None,
+			'arm': 'stable',
+			'arm64': 'exp',
+			'm68k': 'dev',
+		}
+		self._test('alpha arm arm64 m68k', ops,
+		           '~alpha arm ~arm64 ~m68k', arch_status)
+
+
+class TestProcessContent(unittest.TestCase):
+	"""Tests for process_content"""
+
+	def _testKeywords(self, line):
+		ops = (
+			ekeyword.Op(None, 'arm', None),
+			ekeyword.Op('~', 'sparc', None),
+		)
+		return ekeyword.process_content(
+			'file', ['%s\n' % line], ops, quiet=True)
+
+	def testKeywords(self):
+		"""Basic KEYWORDS mod"""
+		updated, ret = self._testKeywords('KEYWORDS=""')
+		self.assertTrue(updated)
+		self.assertEqual(ret, ['KEYWORDS="arm ~sparc"\n'])
+
+	def testKeywordsIndented(self):
+		"""Test KEYWORDS indented by space"""
+		updated, ret = self._testKeywords(' 	 	KEYWORDS=""')
+		self.assertTrue(updated)
+		self.assertEqual(ret, [' 	 	KEYWORDS="arm ~sparc"\n'])
+
+	def testKeywordsSingleQuote(self):
+		"""Test single quoted KEYWORDS"""
+		updated, ret = self._testKeywords("KEYWORDS=' '")
+		self.assertTrue(updated)
+		self.assertEqual(ret, ['KEYWORDS="arm ~sparc"\n'])
+
+	def testKeywordsComment(self):
+		"""Test commented out KEYWORDS"""
+		updated, ret = self._testKeywords('# KEYWORDS=""')
+		self.assertFalse(updated)
+		self.assertEqual(ret, ['# KEYWORDS=""\n'])
+
+	def testKeywordsCode(self):
+		"""Test code leading KEYWORDS"""
+		updated, ret = self._testKeywords('[[ ${PV} ]] && KEYWORDS=""')
+		self.assertTrue(updated)
+		self.assertEqual(ret, ['[[ ${PV} ]] && KEYWORDS="arm ~sparc"\n'])
+
+	def testKeywordsEmpty(self):
+		"""Test KEYWORDS not set at all"""
+		updated, ret = self._testKeywords(' KEYWORDS=')
+		self.assertFalse(updated)
+		self.assertEqual(ret, [' KEYWORDS=\n'])
+
+	def _testSmoke(self, format='color-inline', verbose=False, quiet=False):
+		ops = (
+			ekeyword.Op(None, 'arm', None),
+			ekeyword.Op('~', 'sparc', None),
+		)
+		ekeyword.process_content(
+			'asdf', ['KEYWORDS="arm"'], ops, verbose=verbose,
+			quiet=quiet, format=format)
+
+	def testSmokeQuiet(self):
+		"""Smoke test for quiet mode"""
+		self._testSmoke(quiet=True)
+
+	def testSmokeVerbose(self):
+		"""Smoke test for verbose mode"""
+		self._testSmoke(verbose=True)
+
+	def testSmokeFormatColor(self):
+		"""Smoke test for color-inline format"""
+		self._testSmoke('color-inline')
+
+	def testSmokeFormatInline(self):
+		"""Smoke test for inline format"""
+		self._testSmoke('inline')
+
+	def testSmokeFormatShortMulti(self):
+		"""Smoke test for short-multi format"""
+		self._testSmoke('short-multi')
+
+	def testSmokeFormatLongMulti(self):
+		"""Smoke test for long-multi format"""
+		self._testSmoke('long-multi')
+
+
+class TestProcessEbuild(unittest.TestCase):
+	"""Tests for process_ebuild
+
+	This is fairly light as most code is in process_content.
+	"""
+
+	def _test(self, dry_run):
+		ops = (
+			ekeyword.Op(None, 'arm', None),
+			ekeyword.Op('~', 'sparc', None),
+		)
+		with tempfile.NamedTemporaryFile() as tmp:
+			with open(tmp.name, 'wb') as fw:
+				with open(os.path.join(TESTDIR, 'process-1.ebuild'), 'rb') as f:
+					orig_content = f.read()
+					fw.write(orig_content)
+			ekeyword.process_ebuild(tmp.name, ops, dry_run=dry_run)
+			with open(tmp.name, 'rb') as f:
+				new_content = f.read()
+				if dry_run:
+					self.assertEqual(orig_content, new_content)
+				else:
+					self.assertNotEqual(orig_content, new_content)
+
+	def testSmokeNotDry(self):
+		self._test(False)
+
+	def testSmokeDry(self):
+		self._test(True)
+
+
+class TestLoadProfileData(unittest.TestCase):
+	"""Tests for load_profile_data"""
+
+	def _test(self, subdir):
+		portdir = os.path.join(TESTDIR, 'profiles', subdir)
+		return ekeyword.load_profile_data(portdir=portdir)
+
+	def testLoadBoth(self):
+		"""Test loading both arch.list and profiles.desc"""
+		ret = self._test('both')
+		self.assertIn('arm', ret)
+		self.assertEqual(ret['arm'], 'stable')
+		self.assertIn('arm64', ret)
+		self.assertEqual(ret['arm64'], 'exp')
+
+	def testLoadArchOnly(self):
+		"""Test loading only arch.list"""
+		ret = self._test('arch-only')
+		self.assertIn('arm', ret)
+		self.assertEqual(ret['arm'], None)
+		self.assertIn('x86-solaris', ret)
+
+	def testLoadProfilesOnly(self):
+		"""Test loading only profiles.desc"""
+		ret = self._test('profiles-only')
+		self.assertIn('arm', ret)
+		self.assertEqual(ret['arm'], 'stable')
+		self.assertIn('arm64', ret)
+		self.assertEqual(ret['arm64'], 'exp')
+
+	def testLoadNone(self):
+		"""Test running when neither files exists"""
+		ret = self._test('none')
+		self.assertEqual(ret, {})
+
+
+class TestArgToOps(unittest.TestCase):
+
+	def _test(self, arg, op):
+		self.assertEqual(ekeyword.arg_to_op(arg), ekeyword.Op(*op))
+
+	def testStable(self):
+		self._test('arm', (None, 'arm', None))
+
+	def testUnstable(self):
+		self._test('~ppc64', ('~', 'ppc64', None))
+
+	def testDisabled(self):
+		self._test('-sparc', ('-', 'sparc', None))
+
+	def testDeleted(self):
+		self._test('^x86-fbsd', ('^', 'x86-fbsd', None))
+
+	def testSync(self):
+		self._test('s390=x86', (None, 's390', 'x86'))
+
+
+class TestMain(unittest.TestCase):
+
+	def testSmoke(self):
+		ekeyword.main(['arm', '--dry-run', os.path.join(TESTDIR, 'process-1.ebuild')])
+		ekeyword.main(['--version', '--dry-run'])
+
+
+if __name__ == '__main__':
+	unittest.main()
