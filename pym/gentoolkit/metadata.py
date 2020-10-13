gentoo/gentoolkit

commit 517581df206766fabf10273cde565e0a6dc62829

metadata: Drop deprecated ElementTree methods
Closes: https://github.com/gentoo/gentoolkit/pull/11
Signed-off-by: Georgy Yakovlev <gyakovlev@gentoo.org>

pym/gentoolkit/metadata.py
@@ -73,8 +73,7 @@ class _Maintainer(object):
 		self.description = None
 		self.restrict = node.get('restrict')
 		self.status = node.get('status')
-		maint_attrs = node.getchildren()
-		for attr in maint_attrs:
+		for attr in node.iter():
 			setattr(self, attr.tag, attr.text)
 
 	def __repr__(self):
@@ -101,7 +100,7 @@ class _Useflag(object):
 		_desc = ''
 		if node.text:
 			_desc = node.text
-		for child in node.getchildren():
+		for child in node.iter():
 			_desc += child.text if child.text else ''
 			_desc += child.tail if child.tail else ''
 		# This takes care of tabs and newlines left from the file
@@ -213,7 +212,7 @@ class MetaData(object):
 		if herd in ('no-herd', 'maintainer-wanted', 'maintainer-needed'):
 			return None
 
-		for node in self._herdstree.getiterator('herd'):
+		for node in self._herdstree.iter('herd'):
 			if node.findtext('name') == herd:
 				return node.findtext('email')
 
@@ -283,7 +282,7 @@ class MetaData(object):
 			return self._useflags
 
 		self._useflags = []
-		for node in self._xml_tree.getiterator('flag'):
+		for node in self._xml_tree.iter('flag'):
 			self._useflags.append(_Useflag(node))
 
 		return self._useflags

commit f14b6198d1dd9cb7f4a83f3822e4a1782a5581e8

metadata.py: Fix duplicated use flag text bug 748129
Regression from commit: 517581df206766
link: https://gitweb.gentoo.org/proj/gentoolkit.git/commit/?id=517581df206766fabf10273cde565e0a6dc62829
Gentoo bug: https://bugs.gentoo.org/748129
Signed-off-by: Brian Dolbec <dolsen@gentoo.org>

pym/gentoolkit/metadata.py
@@ -101,8 +101,11 @@ class _Useflag(object):
 		if node.text:
 			_desc = node.text
 		for child in node.iter():
-			_desc += child.text if child.text else ''
-			_desc += child.tail if child.tail else ''
+			# prevent duplicate text
+			if child.text and child.text not in _desc:
+				_desc += child.text
+			if child.tail and not child.tail in _desc:
+				_desc += child.tail
 		# This takes care of tabs and newlines left from the file
 		self.description = re.sub(r'\s+', ' ', _desc)
 