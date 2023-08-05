gentoo/gentoolkit

commit 637bfbfb27d2104a81d80e5c67a7878d2e874966

equery: list: correctly handle installed packages no longer in tree
Closes: https://bugs.gentoo.org/369581
Signed-off-by: Sam James <sam@gentoo.org>

pym/gentoolkit/equery/list_.py
@@ -233,10 +233,10 @@ def main(input_args):
             )
 
             if QUERY_OPTS["in_porttree"] and not QUERY_OPTS["in_overlay"]:
-                if not "P" in pkgstr.location:
+                if not ("P" in pkgstr.location or (QUERY_OPTS['in_installed'] and 'I' in pkgstr.location)):
                     continue
             if QUERY_OPTS["in_overlay"] and not QUERY_OPTS["in_porttree"]:
-                if not "O" in pkgstr.location:
+                if not ("O" in pkgstr.location or (QUERY_OPTS['in_installed'] and 'I' in pkgstr.location)):
                     continue
             pp.uprint(pkgstr)
 

commit 9fbd05c05f86bb9c67d229ea64fe72c5ea08cf62

equery: reformat with black
Fixes: 637bfbfb27d2104a81d80e5c67a7878d2e874966
Signed-off-by: Sam James <sam@gentoo.org>

pym/gentoolkit/equery/list_.py
@@ -233,10 +233,16 @@ def main(input_args):
             )
 
             if QUERY_OPTS["in_porttree"] and not QUERY_OPTS["in_overlay"]:
-                if not ("P" in pkgstr.location or (QUERY_OPTS['in_installed'] and 'I' in pkgstr.location)):
+                if not (
+                    "P" in pkgstr.location
+                    or (QUERY_OPTS["in_installed"] and "I" in pkgstr.location)
+                ):
                     continue
             if QUERY_OPTS["in_overlay"] and not QUERY_OPTS["in_porttree"]:
-                if not ("O" in pkgstr.location or (QUERY_OPTS['in_installed'] and 'I' in pkgstr.location)):
+                if not (
+                    "O" in pkgstr.location
+                    or (QUERY_OPTS["in_installed"] and "I" in pkgstr.location)
+                ):
                     continue
             pp.uprint(pkgstr)
 