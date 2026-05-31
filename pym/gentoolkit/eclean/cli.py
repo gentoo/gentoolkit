gentoo/gentoolkit

commit 979422f50e5b4a79f82d52e83126eeaba1781189

eclean: don't show 'already clean' if we found some invalid binpkgs
Signed-off-by: Sam James <sam@gentoo.org>

pym/gentoolkit/eclean/cli.py
@@ -634,7 +634,7 @@ def doAction(action, options, exclude={}, output=None):
         if not options["quiet"]:
             output.total("normal", clean_size, file_count + len(vcs), verb, action)
     # nothing was found
-    elif not options["quiet"]:
+    elif not options["quiet"] and not invalids:
         output.einfo("Your " + action + " directory was already clean.")
     if saved and not options["quiet"]:
         verb = "would be"

commit 941cda3ee2c001e4d6cdd9929c71fbe629720698

eclean: fix eclean-dist after invalid output change
Fixes: 979422f50e5b4a79f82d52e83126eeaba1781189
Signed-off-by: Sam James <sam@gentoo.org>

pym/gentoolkit/eclean/cli.py
@@ -567,6 +567,7 @@ def doAction(action, options, exclude={}, output=None):
     saved = {}
     deprecated = {}
     vcs = []
+    invalids = None
     # find files to delete, depending on the action
     if not options["quiet"]:
         output.einfo("Building file list for " + action + " cleaning...")